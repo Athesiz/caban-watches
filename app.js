081bc64a2ae1e942bb354b83a91cfc9e74718967
caban <145022815+Athesiz@users.noreply.github.com>
2023-12-03 13:27:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,19 +23,19 @@ const pinNavbarScene = new ScrollMagic.Scene({
 var addressBarHeight = browserHeight - window.innerHeight;
 var realHeight = window.innerHeight - addressBarHeight;
   
-  var newVerticalTranslation = - (602 * 8.28) + ( (828 - realHeight));
+  var newVerticalTranslation = - ((588 * 8.28)+ addressBarHeight) + ( (828 - realHeight));
   document.querySelector('.catalog2 img').style.transform = 'translate(0%, ' + newVerticalTranslation + 'px)';
   
 
-  var newVerticalTranslation2 = - (1068 * 8.28) + ( (828 - realHeight));
+  var newVerticalTranslation2 = - ((1054 * 8.28)+ addressBarHeight) + ( (828 - realHeight));
   document.querySelector('.catalog3 img').style.transform = 'translate(0%, ' + newVerticalTranslation2 + 'px)';
   
   
-  var newVerticalTranslation4 = - (1413 * 8.28) + ( (828 - realHeight));
+  var newVerticalTranslation4 = - ((1398 * 8.28)+ addressBarHeight) + ( (828 - realHeight));
   document.querySelector('.catalog4 img').style.transform = 'translate(0%, ' + newVerticalTranslation4 + 'px)';
   
 
-  var newVerticalTranslation6 = - (1637 * 8.28) + (  (828 - realHeight));
+  var newVerticalTranslation6 = - ((1620 * 8.28)+ addressBarHeight) + (  (828 - realHeight));
   document.querySelector('.catalog5 img').style.transform = 'translate(0%, ' + newVerticalTranslation6 + 'px)';
   
 
