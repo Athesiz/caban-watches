b958be2df1fe6e5e59a7ac73db3bbcde795e96e0
caban <145022815+Athesiz@users.noreply.github.com>
2023-12-04 13:46:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -75,20 +75,20 @@ window.addEventListener('scroll', function () {
   document.querySelector('.grid').style.transform = 'translate(0%, ' + newVerticalTranslation8 + 'px)';
   } 
   else {
-  var newVerticalTranslation = -((realHeight * 5) + urlBarHeight * 5);
+  var newVerticalTranslation = -((realHeight * 5) + ((urlBarHeight * 5));
   document.querySelector('.catalog2 img').style.transform = 'translate(0%, ' + newVerticalTranslation + 'px)';
   document.querySelector('.lng-tbi').style.transform = 'translate(0%, ' + newVerticalTranslation + 'px)';
 
-  var newVerticalTranslation2 = -((realHeight * 9) + urlBarHeight * 9);
+  var newVerticalTranslation2 = -((realHeight * 9) + ((urlBarHeight * 9));
   document.querySelector('.catalog3 img').style.transform = 'translate(0%, ' + newVerticalTranslation2 + 'px)';
   document.querySelector('.lng-4bi').style.transform = 'translate(0%, ' + newVerticalTranslation2 + 'px)';
   
   
-  var newVerticalTranslation4 = -((realHeight * 12) + urlBarHeight * 12);
+  var newVerticalTranslation4 = -((realHeight * 12) + (urlBarHeight * 12));
   document.querySelector('.catalog4 img').style.transform = 'translate(0%, ' + newVerticalTranslation4 + 'px)';
   document.querySelector('.lng-5bi').style.transform = 'translate(0%, ' + newVerticalTranslation4 + 'px)';
 
-  var newVerticalTranslation6 = -((realHeight * 14) + urlBarHeight * 14);
+  var newVerticalTranslation6 = -((realHeight * 14) + (urlBarHeight * 14));
   document.querySelector('.catalog5 img').style.transform = 'translate(0%, ' + newVerticalTranslation6 + 'px)';
   document.querySelector('.lng-6bi').style.transform = 'translate(0%, ' + newVerticalTranslation6 + 'px)';
   
